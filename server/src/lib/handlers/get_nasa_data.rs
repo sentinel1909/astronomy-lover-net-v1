3a5628662fa1bba5218be3f6177ab28d9ce0bf9a
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2025-01-01 21:38:16 -0800

    improvement: add error handling to nasa_data handler

--- server/src/lib/handlers/get_nasa_data.rs
+++ server/src/lib/handlers/get_nasa_data.rs
@@ -1,6 +1,7 @@
 // src/lib/handlers/get_nasa_data.rs
 
 // dependencies
+use crate::errors::ApiError;
 use crate::service::AppState;
 use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
 use domain::NasaData;
@@ -8,25 +9,25 @@ use serde_json::json;
 use url::Url;
 
 // get endpoint handler to retrieve data from the NASA API
-pub async fn from_nasa_api(State(state): State<AppState>) -> impl IntoResponse {
+pub async fn from_nasa_api(State(state): State<AppState>) -> Result<impl IntoResponse, ApiError> {
     let key = state.config.api_key;
-    let api_key = ["apod?api_key=", &key].concat();
-    let api_url = Url::parse("https://api.nasa.gov/planetary/")
-        .unwrap()
-        .join(&api_key)
-        .unwrap();
+    let api_key_segment = ["apod?api_key=", &key].concat();
+    let nasa_api_url = Url::parse("https://api.nasa.gov/planetary/")
+        .map_err(|e| ApiError::Internal(e.to_string()))?
+        .join(&api_key_segment)
+        .map_err(|e| ApiError::Internal(e.to_string()))?;
     let api_client = state.client;
 
     let response = api_client
-        .get(api_url)
+        .get(nasa_api_url)
         .send()
         .await
-        .unwrap()
+        .map_err(|e| ApiError::Internal(e.to_string()))?
         .json::<NasaData>()
         .await
-        .unwrap();
+        .map_err(|e| ApiError::Internal(e.to_string()))?;
 
     let response_body = json!(response);
 
-    (StatusCode::OK, Json(response_body)).into_response()
+    Ok((StatusCode::OK, Json(response_body)).into_response())
 }
--- server/src/lib/lib.rs
+++ server/src/lib/lib.rs
@@ -2,11 +2,13 @@
 
 // module declarations
 pub mod configuration;
+pub mod errors;
 pub mod handlers;
 pub mod service;
 pub mod telemetry;
 
 // re-exports
 pub use configuration::*;
+pub use errors::*;
 pub use service::*;
 pub use telemetry::*;
