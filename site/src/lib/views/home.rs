b365537b1853913ce1598dc8f12e12f59b640569
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2025-01-01 16:04:06 -0800

    improvement: separate frontend from backend and change fetching to call the backend API

--- site/src/lib/views/home.rs
+++ site/src/lib/views/home.rs
@@ -4,11 +4,6 @@
 use domain::NasaData;
 use gloo_net::http::Request;
 use leptos::prelude::*;
-use std::rc::Rc;
-use url::Url;
-
-// declare the NASA API key as a constant
-const NASA_API_KEY: &str = "lsULnkmChaJlS3fZO85M3cnGA8TFCAm2peEfd9QS";
 
 // function to fetch data from the NASA Astronomy Photo of the Day API
 async fn get_api_data(api_url: &str) -> NasaData {
@@ -21,27 +16,14 @@ async fn get_api_data(api_url: &str) -> NasaData {
         .expect("Data received from API is not valid.")
 }
 
-// function to build the API url, including the API key
-fn build_api_url() -> Url {
-    let key = NASA_API_KEY;
-    let api_key = ["apod?api_key=", key].concat();
-    let api_url = Url::parse("https://api.nasa.gov/planetary/")
-        .expect("Failed to parse the url to fetch data from.");
-    api_url
-        .join(&api_key)
-        .expect("Failed to join URL with the API key.")
-}
-
 // function which returns the home or index page view
 #[component]
 pub fn Home() -> impl IntoView {
-    let api_url = Rc::new(build_api_url());
+    let api_url = "http://localhost:8000/api/nasa_data";
     let (nasa_data, set_nasa_data) = signal(NasaData::default());
-    let api_url_clone = Rc::clone(&api_url);
     Effect::new(move |_| {
-        let api_url_clone = Rc::clone(&api_url_clone);
         wasm_bindgen_futures::spawn_local(async move {
-            let fetched_data = get_api_data(api_url_clone.as_ref().as_str()).await;
+            let fetched_data = get_api_data(api_url).await;
             set_nasa_data.set(fetched_data);
         });
     });
