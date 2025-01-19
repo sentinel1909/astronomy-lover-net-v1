ee59d4b201abf14a521a5007c6d69d0a15c35c8d
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2025-01-19 11:24:55 -0800

    improvment: add Turso SQLite database

--- server/src/bin/main.rs
+++ server/src/bin/main.rs
@@ -4,12 +4,18 @@
 use astronomy_lover_net_v1_lib::configuration::AppConfig;
 use astronomy_lover_net_v1_lib::service::{AppState, AstronomyLoverNetApplication};
 use astronomy_lover_net_v1_lib::{get_subscriber, init_subscriber};
+use libsql::Database;
 use reqwest::Client;
 use shuttle_runtime::{Error, SecretStore, Secrets};
+use shuttle_turso::Turso;
+use std::sync::Arc;
 
 // main function
 #[shuttle_runtime::main]
-async fn main(#[Secrets] secrets: SecretStore) -> Result<AstronomyLoverNetApplication, Error> {
+async fn main(
+    #[Secrets] secrets: SecretStore,
+    #[Turso(addr = "{secrets.TURSO_ADDR}", token = "{secrets.TURSO_TOKEN}")] client: Database,
+) -> Result<AstronomyLoverNetApplication, Error> {
     // initialize tracing
     let subscriber = get_subscriber(
         "astronomy-lover-net-v1".into(),
@@ -17,6 +23,13 @@ async fn main(#[Secrets] secrets: SecretStore) -> Result<AstronomyLoverNetApplic
         std::io::stdout,
     );
     init_subscriber(subscriber);
+
+    let client = Arc::new(client);
+    let conn = client.connect().unwrap();
+
+    conn.execute("CREATE TABLE IF NOT EXISTS nasa_api_data (uid text primary key, copyright text, date text, explanation text, hdurl text, media_type text, title text, url text);", ())
+        .await
+        .unwrap();
 
     // get the app configuration, including configuration and reqwest client
     let config = AppConfig::try_from(secrets)?;
